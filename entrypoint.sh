1e8ac085829ebf945ea0a08e72e3e73b66788312
Pedro Costa <pedrof.dacosta@icloud.com>
2022-08-18 18:58:03 +0100

    Fixes output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-version=$(docker run --entrypoint spicedb quay.io/authzed/spicedb:$INPUT_VERSION version)
+output=$(docker run --entrypoint spicedb quay.io/authzed/spicedb:$INPUT_VERSION version)
+version=$(echo $output | awk '{print $NF}')
 echo "::set-output name=version::$version"
