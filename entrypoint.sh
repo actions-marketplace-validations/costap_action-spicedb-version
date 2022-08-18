e0bad1b63570ac04fd6071bf6d0daa330de1c3f1
Pedro Costa <pedrof.dacosta@icloud.com>
2022-08-18 18:52:34 +0100

    Updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-RUN_CMD="docker run --entrypoint spicedb quay.io/authzed/spicedb:$INPUT_VERSION version"
-sh -c "$RUN_CMD"
+version=$(docker run --entrypoint spicedb quay.io/authzed/spicedb:$INPUT_VERSION version)
+echo "::set-output name=version::$version"
